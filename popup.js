d64c594af322f1b877ffad523dcdfa7fa9c771a0
ishnad <mdanishboy@gmail.com>
2025-03-23 21:30:30 +0800

    Revert "flipped"
    This reverts commit 028990d81f8f266b1d61ad88f5e8f6661a028ef9.

--- popup.js
+++ popup.js
@@ -10,7 +10,7 @@ document.addEventListener("DOMContentLoaded", async () => {
                 let silentMode = data.silentMode ?? true;
 
                 toggleButton.textContent = enabled ? "Disable Auto Collect" : "Enable Auto Collect";
-                silentModeButton.textContent = silentMode ? "Silent Mode: ON" : "Silent Mode: OFF";
+                silentModeButton.textContent = silentMode ? "Silent Mode: OFF" : "Silent Mode: ON";
 
                 // Update badge
                 chrome.runtime.sendMessage({ type: "updateBadge" });
