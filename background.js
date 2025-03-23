60f73c9dd9bd0df061f7241f93c35ce4fa0ce00f
ishnad <mdanishboy@gmail.com>
2025-03-23 21:26:10 +0800

    silent mode default: ON

--- background.js
+++ background.js
@@ -2,7 +2,7 @@
 function updateBadge() {
     chrome.storage.local.get(["enabled", "silentMode"], (data) => {
         let enabled = data.enabled ?? true;
-        let silentMode = data.silentMode ?? false;
+        let silentMode = data.silentMode ?? true;
 
         let text = enabled ? "ON" : "OFF";
         let color = enabled ? (silentMode ? [128, 128, 128, 255] : [0, 255, 0, 255]) : [255, 0, 0, 255];
